b2e5699eafc205cc721ed1c44570578c693eed47
Joemel <bituinjoemel@gmail.com>
2021-03-13 22:58:38 +0800

    Remove extension on import statements

--- client/src/components/chat/ChatList.tsx
+++ client/src/components/chat/ChatList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import Chat from './Chat.tsx';
+import Chat from './Chat';
 
 export default function ChatList() {
   const chatList = [
