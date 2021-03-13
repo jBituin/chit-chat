aed5495cdad9db16fa3ab88cac7be0c942521c23
Joemel <bituinjoemel@gmail.com>
2021-03-13 22:56:03 +0800

    WIP Add chat list component

---
+++ client/src/components/chat/ChatList.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import Chat from './Chat.tsx';
+
+export default function ChatList() {
+  const chatList = [
+    {
+      chatName: 'Ariane Nulud',
+      chatImage: 'https://bit.ly/2Z4KKcF',
+      latestChat: 'This is test',
+    },
+    {
+      chatName: 'Yondudes',
+      chatImage: 'https://bit.ly/ryan-florence',
+      latestChat: 'Nakagastos ng 30k',
+    },
+  ];
+
+  return (
+    <Box maxW='sm' borderRadius='lg' overflow='hidden'>
+      {chatList.map(({ chatName, chatImage, latestChat }) => {
+        return (
+          <Chat
+            chatName={chatName}
+            chatImage={chatImage}
+            latestChat={latestChat}
+            key={chatName}
+          />
+        );
+      })}
+    </Box>
+  );
+}
