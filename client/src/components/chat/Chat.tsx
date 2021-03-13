f8a3a2febfe3980c1d76a75ee8c79b2d7be65ace
Joemel <bituinjoemel@gmail.com>
2021-03-13 22:56:14 +0800

    WIP Add chat component

---
+++ client/src/components/chat/Chat.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Avatar, Text, Wrap, WrapItem } from '@chakra-ui/react';
+
+interface Props {
+  chatName: string;
+  chatImage?: string;
+  latestChat: string;
+}
+
+export default function Chat(props: Props) {
+  let { chatName, chatImage, latestChat } = props;
+  return (
+    <Box maxW='sm' borderRadius='lg' overflow='hidden' my={4}>
+      <Wrap>
+        <WrapItem>
+          <Avatar name={chatName} src={chatImage}></Avatar>
+          <Box textAlign='left' ml={3}>
+            <Text> {chatName}</Text>
+            <Text color='grey' fontSize='small'>
+              {' '}
+              {latestChat}
+            </Text>
+          </Box>
+        </WrapItem>
+      </Wrap>
+    </Box>
+  );
+}
