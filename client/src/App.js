177eae91edcb6433309ad5919929ee1252cb1af1
Joemel <bituinjoemel@gmail.com>
2021-03-18 22:43:41 +0800

    Add grid container

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import ChatList from './components/chat/ChatUserList';
+import ChatUserList from './components/chat/ChatUserList';
+import Chat from './components/chat/Chat';
+import { Grid, GridItem, Box } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className='App'>
+    <Box className='App'>
       <header />
-      <ChatList></ChatList>
-    </div>
+      <Grid h='100vh' templateColumns='repeat(5, 1fr)'>
+        <GridItem bg='darkblue' colSpan='1' p={2}>
+          <ChatUserList></ChatUserList>
+        </GridItem>
+        <GridItem bg='darkcyan' colSpan='4'>
+          <Chat></Chat>
+        </GridItem>
+      </Grid>
+    </Box>
   );
 }
 
